f69217b3445b74c05a23bb3ec4f3188874710b6a
MJ <mohamed.aref32@gmail.com>
2021-07-17 16:37:13 -0500

    started the main stucture of the app, finished the login page styling, started the header build

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from "styled-components";
+
+const Header = () => {
+   return (
+      <Nav>
+         
+      </Nav>
+   )
+};
+
+const Nav=styled.nav`
+position:fixed;
+top:0;
+left:0;
+right:0;
+height:70px;
+
+`;
+
+
+export default Header
